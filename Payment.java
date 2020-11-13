ab81acc5f031a5fe3fc0be8473ab488f9b61e5ae
unknown <sean26oneill@gmail.com>
2020-11-13 12:14:20 +0000

    same

---
+++ Payment.java
@@ -0,0 +1,18 @@
+public class Payment {
+
+    public Payment() {
+
+    }
+
+    public int getYear() {
+
+    }
+
+    public boolean getAmountPaid() {
+
+    }
+
+    public double getAmount() {
+
+    }
+}
