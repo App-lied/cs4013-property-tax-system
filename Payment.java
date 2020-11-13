eec28da3dac105437d35084722ce70fb675bdab1
Andrei Smirnou <andrei2002.AS@gmail.com>
2020-11-13 16:51:24 +0000

    payment

---
+++ Payment.java
@@ -0,0 +1,11 @@
+public class Payment {
+    int year;
+    boolean paid;
+    double amount;
+
+    public Payment(int year, boolean paid, double amount) {
+        this.year = year;
+        this.paid = paid;
+        this.amount = amount;
+    }     
+}
