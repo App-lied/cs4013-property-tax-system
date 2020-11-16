e1a61b27e1b903d993345f5253af657307a2230b
andrei-s24 <andrei2002.AS@gmail.com>
2020-11-16 14:33:00 +0000

    Payment now has accessor methods

--- src/Payment.java
+++ src/Payment.java
@@ -1,11 +1,23 @@
 public class Payment {
-    int year;
-    boolean paid;
-    double amount;
+    private int year;
+    private boolean paid;
+    private double amount;
 
     public Payment(int year, boolean paid, double amount) {
         this.year = year;
         this.paid = paid;
         this.amount = amount;
-    }     
+    }
+    
+    public int getYear{
+        return year;
+    }
+
+    public boolean getPaid{
+        return paid;
+    }
+
+    public double amount{
+        return amount;
+    }
 }
