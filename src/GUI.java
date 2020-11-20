3355bb53bcddc973c32cf30334130222034fcf5f
andrei-s24 <andrei2002.AS@gmail.com>
2020-11-20 13:50:13 +0000

    Andrei moment x1

--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,12 @@
-  
-package src;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
+    public void start(Stage newStage){
+        
+    }
 
     public GUI(String[] args) {
         launch(args);
